63044a8225bec7f3fc341aaa8041c526bc37f954
faudi <alexnagornyi17@gmail.com>
2022-11-17 13:05:11 +0300

    first commit

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyB7McFpNCawFDm-O8_9PV7FGsaqrOggFe4",
+    authDomain: "womanup-d8f19.firebaseapp.com",
+    projectId: "womanup-d8f19",
+    storageBucket: "womanup-d8f19.appspot.com",
+    messagingSenderId: "708443070229",
+    appId: "1:708443070229:web:184ba817d7763841a5131c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
